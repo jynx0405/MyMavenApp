13a95d1387ed45831c6f176612e2d3ab6f2dd1c0
jynx0405 <rajaths0405@gmail.com>
2026-04-09 12:17:06 +0530

    fixed packaging to jar

--- src/main/java/com/example/MainClass.java
+++ src/main/java/com/example/MainClass.java
@@ -1,22 +1,7 @@
 package com.example;
-import java.util.Scanner;
 
-public class MainClass {
+public class Main {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        System.out.print("Enter the number of rows: ");
-        int rows = sc.nextInt();
-        sc.close();
-
-
-        for (int i = 1; i <= rows; i++) {
-
-            for (int j = 1; j <= i; j++) {
-                System.out.print("* ");
-            }
-
-            System.out.println();
-        }
+        System.out.println("Hello");
     }
 }
-
